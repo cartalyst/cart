107a5d263f7b798817ad8f014e8b4737b0610949
Bruno Gaspar <brunofgaspar1@gmail.com>
2013-09-02 20:23:18 +0100

    small update on the config.php

--- src/config/config.php
+++ src/config/config.php
@@ -40,7 +40,7 @@ return array(
 	| Session
 	|--------------------------------------------------------------------------
 	|
-	| Configuration specific to the session component of Cart.
+	| Configuration specific to the session component of the Cart.
 	|
 	*/
 
@@ -51,13 +51,26 @@ return array(
 		| Default Session Key
 		|--------------------------------------------------------------------------
 		|
-		| This option allows you to specify the default session key used by Cart.
+		| This option allows you to specify the default session key used by the Cart.
 		|
 		*/
 
 		'key' => 'cartalyst_cart',
 
- 	),
+		/*
+		|--------------------------------------------------------------------------
+		| Session Lifetime
+		|--------------------------------------------------------------------------
+		|
+		| Here you may specify the number of minutes that you wish the session
+		| to be allowed to remain idle for it is expired. If you want them
+		| to immediately expire when the browser closes, set it to zero.
+		|
+		*/
+
+		'ttl' => 120,
+
+	),
 
 	/*
 	|--------------------------------------------------------------------------
