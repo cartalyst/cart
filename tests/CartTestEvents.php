6912f56f51acdfb04a677f05842d0c0a370dfb24
Suhayb El Wardany <me@suw.me>
2014-01-04 22:59:19 +0200

    add events test
    Signed-off-by: Suhayb El Wardany <me@suw.me>

---
+++ tests/CartTestEvents.php
@@ -0,0 +1,136 @@
+<?php namespace Cartalyst\Cart\Tests;
+/**
+ * Part of the Cart package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Cart
+ * @version    1.0.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+use Cartalyst\Cart\Cart;
+use Cartalyst\Conditions\Condition;
+use Mockery as m;
+use PHPUnit_Framework_TestCase;
+
+class CartTestEvents extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Holds the cart instance.
+	 *
+	 * @var \Cartalyst\Cart\Cart
+	 */
+	protected $cart;
+
+	/**
+	 * Holds the dispatcher instance.
+	 *
+	 * @var \Illuminate\Events\Dipstacher
+	 */
+	protected $dispatcher;
+
+	/**
+	 * Close mockery.
+	 *
+	 * @return void
+	 */
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/**
+	 * Setup resources and dependencies
+	 */
+	public function setUp()
+	{
+		$filesystem = new \Illuminate\Filesystem\Filesystem();
+		$fileSessionHandler = new \Illuminate\Session\FileSessionHandler(
+			$filesystem,
+			__DIR__ . '/storage/sessions'
+		);
+
+		$store = new \Illuminate\Session\Store(
+			'cartalyst_cart_session',
+			$fileSessionHandler
+		);
+
+		$session = new \Cartalyst\Cart\Storage\Sessions\IlluminateSession($store);
+
+		$this->dispatcher = m::mock('Illuminate\Events\Dispatcher');
+
+		$this->cart = new Cart($session, $this->dispatcher);
+	}
+
+	public function testAddItemEvent()
+	{
+		$this->dispatcher->shouldReceive('fire')->once()->with('cart.added', m::any());
+
+		$this->cart->add(
+			array(
+				'id'         => 'foobar1',
+				'name'       => 'Foobar 1',
+				'quantity'   => 2,
+				'price'      => 125.00,
+			)
+		);
+	}
+
+	public function testUpdateItemEvent()
+	{
+		$this->dispatcher->shouldReceive('fire')->once()->with('cart.added', m::any());
+		$this->dispatcher->shouldReceive('fire')->once()->with('cart.updated', m::any());
+
+		$this->cart->add(
+			array(
+				array(
+					'id'         => 'foobar2',
+					'name'       => 'Foobar 2',
+					'quantity'   => 3,
+					'price'      => 120.00,
+				),
+			)
+		);
+
+		$this->cart->update('2d2d8cb241842b326ce0e095dbfc4d41', array(
+			'name'       => 'Foo',
+		));
+	}
+
+	public function testDeleteItemEvent()
+	{
+		$this->dispatcher->shouldReceive('fire')->once()->with('cart.added', m::any());
+		$this->dispatcher->shouldReceive('fire')->once()->with('cart.removed', m::any());
+
+		$this->cart->add(
+			array(
+				array(
+					'id'         => 'foobar2',
+					'name'       => 'Foobar 2',
+					'quantity'   => 3,
+					'price'      => 120.00,
+				),
+			)
+		);
+
+		$this->cart->remove('2d2d8cb241842b326ce0e095dbfc4d41');
+	}
+
+	public function testClearCartEvent()
+	{
+		$this->dispatcher->shouldReceive('fire')->once()->with('cart.clear', m::any());
+
+		$this->cart->clear();
+	}
+
+}
