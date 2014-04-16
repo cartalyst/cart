f6a8dbc3a5ad6898612158cc5edb7c6f86fc4a8a
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-04-16 04:59:14 +0100

    fix the service provider
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- src/Laravel/CartServiceProvider.php
+++ src/Laravel/CartServiceProvider.php
@@ -19,7 +19,7 @@
  */
 
 use Cartalyst\Cart\Cart;
-use Cartalyst\Cart\Storage\Sessions\IlluminateSession;
+use Cartalyst\Cart\Storage\IlluminateSession;
 use Illuminate\Support\ServiceProvider;
 
 class CartServiceProvider extends ServiceProvider {
