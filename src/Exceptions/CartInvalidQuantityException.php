b00a11d473900b5ecf0a7d2eea7b36094d32a369
M. Vugteveen <it-can@users.noreply.github.com>
2020-04-11 19:54:47 +0200

    fix: Composer autoload warnings (#73)

--- src/Exceptions/CartInvalidQuantityException.php
+++ src/Exceptions/CartInvalidQuantityException.php
@@ -22,18 +22,6 @@ namespace Cartalyst\Cart\Exceptions;
 
 use Exception;
 
-class CartInvalidAttributesException extends Exception
-{
-}
-class CartInvalidPriceException extends Exception
-{
-}
 class CartInvalidQuantityException extends Exception
-{
-}
-class CartItemNotFoundException extends Exception
-{
-}
-class CartMissingRequiredIndexException extends Exception
 {
 }
