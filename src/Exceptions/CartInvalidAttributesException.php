c19e80fba47c0992858fd164b0ab7ca4855ffa35
Michiel Vugteveen <info@it-can.nl>
2020-04-11 19:52:15 +0200

    fix issue #72

---
+++ src/Exceptions/CartInvalidAttributesException.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Part of the Cart package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Cart
+ * @version    4.0.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2020, Cartalyst LLC
+ * @link       https://cartalyst.com
+ */
+
+namespace Cartalyst\Cart\Exceptions;
+
+use Exception;
+
+class CartInvalidAttributesException extends Exception
+{
+}
